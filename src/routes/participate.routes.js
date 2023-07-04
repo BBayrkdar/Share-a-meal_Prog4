b8b71e76589beef3250a26ff4cce8e4f97985c37
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-07-04 20:13:09 +0200

    participate routes added

---
+++ src/routes/participate.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const partiController = require("../controller/participate.controller");
+const loginController = require("../controller/login.controller");
+
+router.post("/:mealId/participate", loginController.validateToken, partiController.signUpforMeal);
+
+router.get("/:mealId/participants", partiController.getAllParticipants);
+
+router.get("/:mealId/participants", partiController.getParticipantById);
+
+router.delete("/:mealId/participate", loginController.validateToken, partiController.unregisterforMeal);
+
+module.exports = router;
