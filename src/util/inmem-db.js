bcf46908dcfbf1f05544333c3d3028de962c8efb
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-07-04 20:11:50 +0200

    utils created to some js lib

---
+++ src/util/inmem-db.js
@@ -0,0 +1,28 @@
+let database = {
+    users: [
+        {
+            id: 0,
+            firstName: "Baraa",
+            lastName: "Bayrkdar",
+            street: "Teststreet 1",
+            city: "TestCity",
+            isActive: true,
+            email: "test@mail.com",
+            password: "secret",
+            phoneNumber: "06 12345678"
+        },
+        {
+            id: 1,
+            firstName: "Lily",
+            lastName: "Kurdi",
+            street: "Teststreet 11",
+            city: "TestCity",
+            isActive: true,
+            email: "lily@mail.com",
+            password: "secret",
+            phoneNumber: "06 12345687"
+        },
+    ],
+};
+
+module.exports = database;
